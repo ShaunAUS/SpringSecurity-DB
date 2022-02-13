77858fc064508536b860d198529ca8b7734c447d
ShaunAUS <86937655+ShaunAUS@users.noreply.github.com>
2022-02-14 00:21:46 +0900

    v4
    인가처리 허용필터 -permitAllFilter 구현

---
+++ core-spring-securityV4/src/main/java/corespringsecurity/config/AppConfig.java
@@ -0,0 +1,23 @@
+package corespringsecurity.config;
+
+
+import corespringsecurity.repository.ResourceRepository;
+import corespringsecurity.service.SecurityResourceService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+//공통적으로쓰는 bean
+@Configuration
+public class AppConfig {
+
+    //DB에서 가져와야하니 레파지토리 필요
+    @Bean
+    public SecurityResourceService securityResourceService(ResourceRepository resourceRepository){
+
+        SecurityResourceService securityResourceService = new SecurityResourceService(resourceRepository);
+
+        return securityResourceService;
+
+    }
+}
